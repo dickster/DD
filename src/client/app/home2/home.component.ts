30bbe0b635c09583ba4ce1790d38ba6815684e04
dickster <derek.ipod@gmail.com>
2017-06-07 18:02:57 -0400

    backup home directory.

--- src/client/app/home2/home.component.ts
+++ src/client/app/home2/home.component.ts
@@ -10,7 +10,7 @@ import { NameListService } from '../shared/name-list/name-list.service';
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements On  Init {
 
   newName: string = '';
   errorMessage: string;
